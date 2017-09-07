e630840debbe732d3100cdcb2211d78dc0216720
mgalindor <mgldrz@gmailcom>
2017-09-07 10:57:54 -0500

    Se agrega servidor eureka se quitan properties y se agregan yaml

--- mServices/fnNationalities/src/main/java/com/mk/mnx/StartUpService.java
+++ mServices/fnNationalities/src/main/java/com/mk/mnx/StartUpService.java
@@ -2,9 +2,11 @@ package com.mk.mnx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ImportResource("classpath:application-context.xml")
 public class StartUpService {
 
