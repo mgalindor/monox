294b65f364068ec4c45d499db6023410a4a27380
mgalindor <mgldrz@gmailcom>
2017-09-22 13:28:13 -0500

    Se cambia el almacen del usuario (de sesion a request) para el caso de autenticacion

--- infra/src/main/java/com/mk/mnx/infr/controller/BaseRestController.java
+++ infra/src/main/java/com/mk/mnx/infr/controller/BaseRestController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +22,7 @@ public abstract class BaseRestController {
 	private HttpServletRequest request;
     
     protected String getUser() {	
-    	HttpSession session = request.getSession(true);
-		return (String) session.getAttribute(CommonConstants.SESSION_USER);
+		return (String) request.getAttribute(CommonConstants.SESSION_USER);
 	}
     
     @ExceptionHandler(Exception.class)
--- mServices/restSupport/src/main/java/com/mk/mnx/filter/SecurityFilter.java
+++ mServices/restSupport/src/main/java/com/mk/mnx/filter/SecurityFilter.java
@@ -10,7 +10,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,8 +47,7 @@ public class SecurityFilter implements Filter {
 					.setSigningKey(CommonConstants.TOKEN_PASS)
 					.parseClaimsJws(tokem).getBody().getSubject();
 					logger.debug("User [{}]",user);
-					HttpSession session = httpRequest.getSession(true);
-					session.setAttribute(CommonConstants.SESSION_USER, user);
+					httpRequest.setAttribute(CommonConstants.SESSION_USER, user);
 				}
 				catch(Exception e) {
 					logger.error("Error añ validar el token ",e);
